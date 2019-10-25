7924b44836e8bffd32daa582456daf2f5f63300d
LeslieNock <leslie_nock@hotmail.co.uk>
2019-10-25 16:19:29 +0100

    Add files via upload

---
+++ Enigma.Tests/GivenAPlugboard.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Enigma.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class GivenAPlugboard
+    {
+        private Plugboard _subject;
+
+        [OneTimeSetUp]
+        public void WhenNoConfigurationHasBeenDefined()
+        {
+            _subject = new Plugboard();
+        }
+
+        [Test]
+        public void ThenTheUsersInputIsNotModified()
+        {
+            Assert.That(_subject.Process('A'), Is.EqualTo('A'));
+        }
+    }
+}
---
+++ Enigma/Plugboard.cs
@@ -0,0 +1,14 @@
+﻿namespace Enigma
+{
+    public class Plugboard
+    {
+        public Plugboard(string configuration = "")
+        {
+        }
+
+        public char Process(char input)
+        {
+            return ' ';
+        }
+    }
+}
